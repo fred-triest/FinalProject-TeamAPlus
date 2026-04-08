80f6e57e0a8334f75ea140e2afc13e8c55e3ddee
fred-triest <triest.f@northeastern.edu>
2026-04-08 14:32:53 -0400

    Admin work panels completed

--- FinalProject-TeamAPlus/src/ui/EnterpriseAdmin/EnterpriseAdminWorkAreaJPanel.java
+++ FinalProject-TeamAPlus/src/ui/EnterpriseAdmin/EnterpriseAdminWorkAreaJPanel.java
@@ -7,7 +7,9 @@ package ui.EnterpriseAdmin;
 import Airport.AirportEcoSystem;
 import Airport.Enterprise.Enterprise;
 import Airport.Organization.Organization;
+import Airport.Role.EnterpriseAdminRole;
 import Airport.UserAccount.UserAccount;
+import java.awt.CardLayout;
 import javax.swing.JPanel;
 
 /**
@@ -16,21 +18,21 @@ import javax.swing.JPanel;
  */
 public class EnterpriseAdminWorkAreaJPanel extends javax.swing.JPanel {
 
-    JPanel container;
-    UserAccount account;
-    Organization organization;
-    Enterprise enterprise;
-    AirportEcoSystem airport;
+    private JPanel container;
+    private UserAccount account;
+    private Organization organization;
+    private Enterprise enterprise;
+    private AirportEcoSystem airport;
     /**
      * Creates new form EnterpriseAdminWorkAreaJPanel
      */
-    public EnterpriseAdminWorkAreaJPanel(JPanel container, UserAccount account, Organization organization, Enterprise enterprise, AirportEcoSystem airport) {
+    public EnterpriseAdminWorkAreaJPanel(JPanel container, UserAccount account, Organization organization, AirportEcoSystem airport) {
         initComponents();
         this.container = container;
         this.account = account;
         this.organization = organization;
-        this.enterprise = enterprise;
         this.airport = airport;
+        this.enterprise = ((EnterpriseAdminRole) account.getRole()).getEnterprise();
     }
 
     /**
@@ -43,9 +45,25 @@ public class EnterpriseAdminWorkAreaJPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         lblTitle = new javax.swing.JLabel();
+        btnManageOrganizations = new javax.swing.JButton();
+        btnManageUsers = new javax.swing.JButton();
 
         lblTitle.setFont(new java.awt.Font("Helvetica Neue", 0, 36)); // NOI18N
         lblTitle.setText("Enterprise Admin Work Area ");
+
+        btnManageOrganizations.setText("Manage Organizations");
+        btnManageOrganizations.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageOrganizationsActionPerformed(evt);
+            }
+        });
+
+        btnManageUsers.setText("Manage Users");
+        btnManageUsers.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageUsersActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -55,18 +73,48 @@ public class EnterpriseAdminWorkAreaJPanel extends javax.swing.JPanel {
                 .addGap(357, 357, 357)
                 .addComponent(lblTitle)
                 .addContainerGap(380, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnManageUsers, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnManageOrganizations, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(426, 426, 426))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(37, 37, 37)
                 .addComponent(lblTitle)
-                .addContainerGap(765, Short.MAX_VALUE))
+                .addGap(61, 61, 61)
+                .addComponent(btnManageOrganizations, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnManageUsers, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(592, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnManageOrganizationsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageOrganizationsActionPerformed
+        
+        ManageOrganizationsJPanel manageOrganizationsJPanel = new ManageOrganizationsJPanel(container, enterprise, airport);
+        container.add("manageOrganizationsJPanel", manageOrganizationsJPanel);
+        CardLayout layout = (CardLayout) container.getLayout();
+        layout.show(container, "manageOrganizationsJPanel");
+
+    }//GEN-LAST:event_btnManageOrganizationsActionPerformed
+
+    private void btnManageUsersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageUsersActionPerformed
+        
+        ManageUsersJPanel manageUsersJPanel = new ManageUsersJPanel(container, enterprise, airport);
+        container.add("manageUsersJPanel", manageUsersJPanel);
+        CardLayout layout = (CardLayout) container.getLayout();
+        layout.show(container, "manageUsersJPanel");
+
+    }//GEN-LAST:event_btnManageUsersActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnManageOrganizations;
+    private javax.swing.JButton btnManageUsers;
     private javax.swing.JLabel lblTitle;
     // End of variables declaration//GEN-END:variables
 }
