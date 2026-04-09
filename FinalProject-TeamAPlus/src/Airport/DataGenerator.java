5ffde2175669065defc25845e70a97a6cc85094b
Larry Tsao <larrytsao6@gmail.com>
2026-04-08 22:37:40 -0700

    create DataGenerator class

---
+++ FinalProject-TeamAPlus/src/Airport/DataGenerator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Airport;
+
+import com.github.javafaker.Faker;
+
+
+/**
+ * DataGenerator - Populates the system with realistic fake employee data
+ * @author Larry Tsao (wakingstardust)
+ */
+public class DataGenerator {
+    
+    public static void generate(AirportEcoSystem system) {
+        Faker faker = new Faker();
+    }
+    
+}
